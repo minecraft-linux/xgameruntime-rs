5b5ed19f3d196c2b219510b2c6a5899d46ed7b69
Christopher Homberger <christopher.homberger@web.de>
2026-08-23 22:37:53 +0200

    fix: xuser experiement via static xodus lib
    * fix xpersistentlocalstorage regression of iid
    * msaappid read from config
    * titleid read from config

--- src/xpersistedlocalstorage.rs
+++ src/xpersistedlocalstorage.rs
@@ -12,9 +12,7 @@ pub struct XPersistentLocalStorageSpaceInfo {
     pub total_bytes: u64,
 }
 
-// Class _GUID_f4faf4d4_2d04_4fce_b3e0_474a713a3e84
-// IID _GUID_f4faf4d4_2d04_4fce_b3e0_474a713a3e84
-#[interface("f4faf4d4-2d04-4fce-b3e0-474a713a3e84")]
+#[interface("41a4e10c-5a7e-41d9-8c37-37bde62a07d6")]
 pub unsafe trait IXPersistentLocalStorage: IUnknown {
     // XPersistentLocalStorageGetPathSize
     pub unsafe fn x_persistent_local_storage_get_path_size(
